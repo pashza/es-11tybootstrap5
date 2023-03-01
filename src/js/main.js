faf43a530b59e3ae60c90464094516b0f6ac8088
pashza <92363402+pashza@users.noreply.github.com>
2023-03-01 11:18:02 -0800

    added nightmode and navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
 // Bootstrap JS
 var bootstrap = require('bootstrap');
+
+// night-mode nav bar
+const nightModeToggle = document.getElementById('night-mode-toggle');
+const body = document.body;
+
+nightModeToggle.addEventListener('click', function() {
+  body.classList.toggle('night-mode');
+});
